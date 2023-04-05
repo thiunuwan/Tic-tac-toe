66040f884b7cb592def5196e66115d89b22fb348
thiunuwan <ravinduthiunuwan@gmail.com>
2023-04-05 11:00:00 +0530

    setup basic structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {Level1Component} from "./level1/level1.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    pathMatch:"full",
+    redirectTo:"level1"
+  },
+  {
+    path:"level1",
+    component:Level1Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Level1Component } from './level1/level1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Level1Component
   ],
   imports: [
     BrowserModule,
