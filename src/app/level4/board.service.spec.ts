6b35c1280c540ac7f35eb80788592205e15b49cc
thiunuwan <ravinduthiunuwan@gmail.com>
2023-04-06 22:13:38 +0530

    update - add test file

--- src/app/level4/board.service.spec.ts
+++ src/app/level4/board.service.spec.ts
@@ -3,14 +3,42 @@ import { TestBed } from '@angular/core/testing';
 import { BoardService } from './board.service';
 
 describe('BoardService', () => {
-  let service: BoardService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(BoardService);
+  it('can be compare two numbers',() =>{
+    expect(1).toBe(1);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should set  pieces in the board', ()=> {
+    const board=new BoardService();
+    board.set(0,0);
+    expect(board.boardContent[0][0]).toBe(1);
+
+    board.set(1,0);
+    expect(board.boardContent[0][1]).toBe(2);
+
   });
+
+  // xit    to temporarily disable tests
+  it('should not set multiple set-operations in same cell ',  () => {
+    const board=new BoardService();
+    board.set(0,0);
+    board.set(0,0);
+    expect(board.boardContent[0][0]).toBe(1);
+
+    board.set(1,0);
+    board.set(1,0);
+    expect(board.boardContent[0][1]).toBe(2);
+
+  });
+
+  it('should correctly identify the winner when a row is filled by same player',  () => {
+    const board=new BoardService();
+    board.set(0,0);
+    board.set(1,0);
+    board.set(0,1);
+    board.set(1,1);
+    board.set(0,2);
+    expect(board.winnerIndex).toBe(1);
+  });
+
+
 });
--- src/app/level5/player-name-into-class.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PlayerNameIntoClassPipe } from './player-name-into-class.pipe';
-
-describe('PlayerNameIntoClassPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PlayerNameIntoClassPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
